14b27f09735d3940cc02853fe659a729b49c8208
royermatt <royer.matt@gmail.com>
2021-02-18 22:02:43 -0500

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,57 +1,116 @@
-(function() {
+var modal = new bootstrap.Modal(document.getElementById('modal'), {
+    keyboard: false
+});
 
-    $(document).ready(function() {
-        console.log('ready');
+var data = [];
 
-        $.ajax({
-            url: "https://raw.githubusercontent.com/royermatt/compassiongroups/main/groups.json",
-            method: "GET",
-            success: function(r) {
-                var json = JSON.parse(r);
 
-                $.each(json.data, function(i, v) {
+$(document).ready(function() {
 
-                    var tags = '';
+    $.ajax({
+        url: "https://raw.githubusercontent.com/royermatt/compassiongroups/main/groups.json",
+        method: "GET",
+        success: function(r) {
+            var json = JSON.parse(r);
+            data = json.data || [];
 
-                    if(v.online) {
-                        tags += '<span class="badge">Online</span>';
-                    }
-                    
-                    if(v.childcare) {
-                        tags += '<span class="badge">Childcare</span>';
-                    } 
+            $.each(data, function(i, v) {
 
-                    if(v.full) {
-                        tags += '<div class="full">This group is currently full</div>';
-                    } else {
-                        tags += '<button type="button" class="btn btn-sm">Join group</button>'
-                    }
+                var tags = '';
 
-                    var description = v.description;
-                    if(description.length > 120) {
-                        description = description.substring(0,120).trim() + "...";
-                    }
+                if(v.online) {
+                    tags += '<span class="badge">Online</span>';
+                }
+                
+                if(v.childcare) {
+                    tags += '<span class="badge">Childcare</span>';
+                } 
 
-                    var html = [
-                        '<div class="group card">',
-                            '<div class="card-body>',
-                                '<span class="icon circle"></span>',
-                                '<h4 class="card-title">' + v.name + '</h4>',
-                                '<p class="card-subtitle mb-2 text-muted">' + v.day + ' at ' + v.time + '</p>',
-                                '<div class="type">' + v.type + '</div>',
-                                '<div class="card-text">',
-                                    '<p class="coach">Coach: ' + v.coach + '</p>',
-                                    '<p class="description">' + description + '</p>',
-                                '</div>',
-                                tags,
+                if(v.full) {
+                    tags += '<div class="full">This group is currently full</div>';
+                }
+
+                var description = v.description;
+                if(description.length > 120) {
+                    description = description.substring(0,120).trim() + "...";
+                }
+
+                var html = [
+                    '<div class="group card" onclick="openModal('+ i +')">',
+                        '<div class="card-header">',
+                            '<h4 class="card-title">' + v.name + '</h4>',
+                            '<span class="card-subtitle mb-2">' + v.day + ' at ' + v.time + '</span>',
+                        '</div>',
+                        '<div class="card-body">',
+                            '<div class="type">' + v.type + '</div>',
+                            '<div class="card-text">',
+                                '<p class="coach">Leader: ' + v.leader + '</p>',
+                                '<p class="description">' + description + '</p>',
                             '</div>',
-                        '</div>'
-                    ];
+                            tags,
+                        '</div>',
+                    '</div>'
+                ];
 
-                    $('#groups').append(html.join(''));
-                });
-            }
-        });
+                $('#groups').append(html.join(''));
+            });
+        }
     });
+});
 
-})();
+var openModal = function(index) {
+
+    var item = data[index];
+
+    var html = [
+        '<h3>' + item.name + '</h3>',
+        '<p>' + item.description + '</p>',
+        '<p>',
+            '<strong>Campus</strong><br />',
+            '<span>' + item.campus + '</span>',
+        '</p>',
+        '<p>',
+            '<strong>Community Type</strong><br />',
+            '<span>' + item.type + '</span>',
+        '</p>',
+        '<p>',
+            '<strong>Location</strong><br />',
+            '<span>' + item.location + '</span>',
+        '</p>'
+    ];
+
+    $("#modal .modal-body").html(html.join(''));
+
+    if($("#modal .modal-footer").length === 0) {  
+        $("#modal .modal-content").append('<div class="modal-footer"><button class="btn" onclick="showRegisterForm()">Join this group</button></div>');
+    }
+    modal.show();
+};
+
+var showRegisterForm = function() {
+    var html = [    
+        '<h5>Join this group</h5>',
+        '<p>Are you interested in joining this group? Use the form below to send an email to the group leader.</p>',
+        '<form style="margin-top:15px;">',
+        '<div class="mb-3 row">',
+            '<label for="" class="form-label">Name</label>',
+              '<div class="input-group">',
+              '<input type="text" aria-label="First name" class="form-control" placeholder="First" />',
+              '<input type="text" aria-label="Last name" class="form-control" placeholder="Last" />',
+            '</div>',
+          '</div>',
+          '<div class="mb-3 row">',
+              '<label for="" class="form-label">Email address</label>',
+              '<div class="input-group">',
+              '<input type="email" class="form-control" placeholder="example@example.com" />',
+              '</div>',
+            '</div>',
+            '<button type="submit" class="btn">Register</button>',
+        '</form>'
+    ];
+
+    $("#modal .modal-body").html(html.join(''));
+    $("#modal .modal-footer").remove();
+    modal.show();
+
+}
