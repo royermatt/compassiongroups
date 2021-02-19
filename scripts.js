d2151ce81922a7c0e0926f179ba57f9452c9bf3a
royermatt <royer.matt@gmail.com>
2021-02-18 19:49:57 -0500

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -2,6 +2,23 @@
 
     $(document).ready(function() {
         console.log('ready');
+
+        $.ajax({
+            url: "https://raw.githubusercontent.com/royermatt/compassiongroups/main/groups.json",
+            method: "GET",
+            success: function(r) {
+                var json = JSON.parse(r);
+
+                $.each(json.data, function(i, v) {
+                    var html = '<div class="group">';
+                        html += '<h3 class="title">' + v.name + '</h3>';
+                        html += '<span class="time">' + v.day + ', ' + v.time + '</span>';
+                        html += '<span class="tag">' + v.type + '</span>';
+                    html += '</div>';
+                    $('#groups').append(html);
+                });
+            }
+        });
     });
 
 })();
