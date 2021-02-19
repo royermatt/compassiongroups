a07a3fe991640c69569aa9261c9cae7c13ff055c
royermatt <royer.matt@gmail.com>
2021-02-18 20:31:04 -0500

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -10,12 +10,36 @@
                 var json = JSON.parse(r);
 
                 $.each(json.data, function(i, v) {
-                    var html = '<div class="group">';
-                        html += '<h3 class="title">' + v.name + '</h3>';
-                        html += '<span class="time">' + v.day + ', ' + v.time + '</span>';
-                        html += '<span class="tag">' + v.type + '</span>';
-                    html += '</div>';
-                    $('#groups').append(html);
+
+                    var tags = '';
+
+                    if(v.online) {
+                        tags += '<span class="badge">Online</span>';
+                    }
+                    
+                    if(v.childcare) {
+                        tags += '<span class="badge">Childcare</span>';
+                    } else {
+                        tags += '<span class="badge">No childcare</span>';
+                    }
+
+                    var html = [
+                        '<div class="group card">',
+                            '<div class="card-body>',
+                                '<span class="icon circle"></span>',
+                                '<h3 class="card-title">' + v.name + '</h3>',
+                                '<p class="card-subtitle mb-2 text-muted">' + v.day + ', ' + v.time + '</p>',
+                                '<div class="type">' + v.type + '</div>',
+                                '<div class="card-text">',
+                                    '<p class="coach">Coach: ' + v.coach + '</p>',
+                                    '<p class="description">' + v.description + '</p>',
+                                '</div>',
+                                tags,
+                            '</div>',
+                        '</div>'
+                    ];
+
+                    $('#groups').append(html.join(''));
                 });
             }
         });
